07b3f0db6230232d04d277583a7743bdf696d1d3
jzzu <jossuedutan@gmail.com>
2018-12-13 12:06:08 -0500

    mostrar, borrar

--- examen/app/src/main/java/com/example/jossue/examen_moviles/EditarActivity.kt
+++ examen/app/src/main/java/com/example/jossue/examen_moviles/EditarActivity.kt
@@ -15,12 +15,20 @@ class EditarActivity : AppCompatActivity() {
         val casa = intent.getParcelableExtra<Casa?>("casa")
         val pos = intent.getIntExtra("pos", 0)
 
+        if (casa != null) {
+            mostrarCampos(casa)
+        }
+
         Log.i("intent-nombre-apellido", "casa actualizar ${casa.toString()}")
 
         button_actualizar.setOnClickListener {
             this.ActualizarCasa(id_editar.text.toString(), desc_editar.text.toString(), m2_editar.text.toString(), precio_editar.text.toString(), pos)
             Log.i("intent-nombre-apellido", "id editar ${id_editar.text.toString()}")
 
+            this.irAListar()
+        }
+        button_eliminar.setOnClickListener {
+            this.BorrarCasa(pos)
             this.irAListar()
         }
     }
@@ -33,12 +41,24 @@ class EditarActivity : AppCompatActivity() {
 
         Log.i("intent-nombre-apellido", "CASA ${BDD.Casa[pos].toString()}")
     }
+
+    fun BorrarCasa (pos:Int):Unit{
+
+
+
+        Log.i("intent-nombre-apellido", "pos ${pos}")
+        BDD.Casa.remove(BDD.Casa[pos])
+
+    }
     fun irAListar(){
         val intentListar = Intent(this, ListarActivity::class.java);
         this.startActivity(intentListar)
     }
     fun mostrarCampos(casa: Casa){
-        id_editar.text = casa.numeroCasa;
+        id_editar.setText(casa.numeroCasa);
+        desc_editar.setText(casa.descripcion);
+        m2_editar.setText(casa.m2);
+        precio_editar.setText(casa.precio);
     }
 
 }
